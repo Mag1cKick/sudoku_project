ae9e777fe5e7575dd18b853afc83adc261fe69aa
NazarDizhak <dizhak.pn@ucu.edu.ua>
2024-05-13 22:19:09 +0300

    webpage_demo

---
+++ maze/maze_solver.py
@@ -0,0 +1,79 @@
+from flask import Flask, request, jsonify, render_template
+app = Flask(__name__)
+
+
+class Maze:
+    def __init__(self, grid) -> None:
+        self.size = len(grid)
+        self.maze = grid
+
+    def exploreMaze(self, row, col):
+
+        if self.maze[row][col]==2:
+            #We found the exit
+            return True
+
+        elif self.maze[row][col]==0: #Empty path, not explored
+            self.maze[row][col]=3
+
+            if row<len(self.maze)-1:
+                #Explore path below
+                if self.exploreMaze(row+1,col):
+                    return True
+
+            if row>0:
+                #Explore path above
+                if self.exploreMaze(row-1,col):
+                    return True
+
+            if col<len(self.maze[row])-1:
+                #Explore path to the right
+                if self.exploreMaze(row,col+1):
+                    return True
+
+            if col>0:
+                #Explore path to the left
+                if self.exploreMaze(row,col-1):
+                    return True
+
+            #Backtrack
+            self.maze[row][col]=4
+
+            # print("Backtrack")
+
+maze_3 = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+          [1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+          [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1],
+          [1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1],
+          [1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1],
+          [1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1],
+          [1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1],
+          [1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1],
+          [1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1],
+          [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1],
+          [1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1],
+          [1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1],
+          [1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1],
+          [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+          [1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+
+maze_solver = Maze(maze_3)
+# print(maze_solver.exploreMaze(1, 1))
+
+@app.route('/', methods = ["GET"])
+def index():
+    return render_template('maze.html')
+
+@app.route("/maze_solver", methods=["POST"])
+def solve_maze_route():
+    maze = request.json.get("maze")
+    if not maze:
+        return jsonify({"error": "No maze data provided"}), 400
+
+    # Call your Python maze solving function
+    is_solvable = maze_solver.exploreMaze(1, 1)
+
+    return jsonify({"solvable": is_solvable, "maze": maze_solver.maze})
+
+if __name__ == "__main__":
+    app.run(debug=True)
